87a07b9b66c6b1a5527d3edabe934a068dc2d76e
vaibhavpali <vaibhavpali@gmail.com>
2021-08-12 16:24:24 +0530

    Initial commit

---
+++ diceroller/src/main/java/com/example/vaibhav/diceroller/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.vaibhav.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.btn_roll_dice)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.btn_count_up)
+        countButton.setOnClickListener { countUp() }
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.txt_hello_world)
+        if(resultText.text.contains("Hello")) {
+            resultText.text = "1";
+        } else {
+            var tempValue = resultText.text.toString().toInt()
+            if(tempValue < 6) {
+                tempValue++
+                resultText.text = tempValue.toString()
+            }
+        }
+    }
+
+    private fun rollDice() {
+        val txtResult: TextView = findViewById(R.id.txt_hello_world)
+        val randomNumber = (1..6).random()
+        txtResult.text = randomNumber.toString()
+    }
+}
